bccfaeb8ead129d89a5ecd8983ce8f963fc0192e
lfalcaolopes <lfalcaolopes@gmail.com>
2023-11-21 01:05:17 -0300

    fix(backend): fix cors validation

--- backend/TodoApp.API/Program.cs
+++ backend/TodoApp.API/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: allowSpecificOrigins,
         policy =>
         {
-            policy.WithOrigins("http://127.0.0.1:5173")
+            policy.WithOrigins("http://localhost:5173")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
