503ee8b151883fcc7d6f2ab8dd7bba0a33abd724
Lucas Lopes <lfalcaolopes@gmail.com>
2023-12-21 00:15:33 -0300

    feat(frontend): add search bar logic to search by todotask name

--- frontend/src/atoms/SearchBar/index.tsx
+++ frontend/src/atoms/SearchBar/index.tsx
@@ -1,10 +1,45 @@
+import { useForm } from 'react-hook-form';
 import * as Styled from './styles';
 import {MagnifyingGlass} from "@phosphor-icons/react";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import api from '../../utils/Axios';
+import { useContext } from 'react';
+import { DataContext } from '../../utils/dataContext';
+import { ToastContext } from '../../utils/toastContext';
+
+const searchSchema = z.object({
+  search: z.string().min(1)
+});
+
+type searchProps = z.infer<typeof searchSchema>;
 
 const SearchBar = () => {
+
+  const { setTodoTaskData } = useContext(DataContext);
+  const { addToast } = useContext(ToastContext);
+  const { register, handleSubmit, reset } = useForm<searchProps>({
+    resolver: zodResolver(searchSchema)
+  });
+
+  function onSubmit(data: searchProps) {
+    api.get(`/todotasks/search?name=${data.search}`).then(response => {
+      if (response.data.success) {
+        setTodoTaskData(response.data.data);
+      } else {
+        addToast({type: "error", message: "Atividade não encontrada" });
+      }
+    });
+    
+    reset();
+  }
+
   return (
-    <Styled.Form>
-      <Styled.Input placeholder="Pesquisar..."/>
+    <Styled.Form onSubmit={handleSubmit(onSubmit)}>
+      <Styled.Input 
+        placeholder="Pesquisar..."
+        {...register("search")}
+        />
 
       <Styled.Button>
         <MagnifyingGlass size={24} weight="bold"/>
